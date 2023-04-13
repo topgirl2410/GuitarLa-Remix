1cf06c7a934f9ec7240997a676d878d9a78341b5
topgirl2410 <maria.topgirl82@gmail.com>
2023-04-13 23:48:52 +0200

    first commit

---
+++ app/root.jsx
@@ -0,0 +1,60 @@
+import {
+  Links,
+  Meta,
+  Outlet,
+} from "@remix-run/react";
+
+import styles from './styles/index.css'
+
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: "https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+    },
+    {
+      rel: "stylesheet",
+      href: styles
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com"
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.gstatic.com",
+      crossOrigin: "true"
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap"
+    }
+  ]
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  )
+}
+
+
+export function Document({ children }) {
+  return (
+    <html lang="es">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/_index.jsx
@@ -0,0 +1,9 @@
+export const meta = () => {
+  return [{ title: "GuitarLA - Remix" }];
+};
+
+export default function Index() {
+  return (
+    <h1>desde index.jsx</h1>
+  );
+}
